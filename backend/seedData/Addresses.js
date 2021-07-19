5872cb4a40dc13266aaa409579ea516383511f0d
Monte <36746765+theflaggship@users.noreply.github.com>
2021-07-19 16:33:30 -0700

    lodgings and addresses added

--- backend/seedData/Addresses.js
+++ backend/seedData/Addresses.js
@@ -1,4 +1,13 @@
 // {addressLineOne: '', addressLineTwo: '', city: '', state: '', postalCode: '', country: ""}
-//1 {addressLineOne: "26600 Oliver Road", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93923", country: "United States"}
-//2 {addressLineOne: "20 Via Contenta", addressLineTwo: null, city: "Carmel Valley", state: "CA", postalCode: "93924", country: "United States"}
-//3 {addressLineOne: "10 Flight Rd", addressLineTwo: null, city: "Carmel Valley", state: "CA", postalCode: "93924", country: "United States"}
+//1 { addressLineOne: "26600 Oliver Rd", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93923", country: "United States" }
+//2 { addressLineOne: "20 Via Contenta", addressLineTwo: null, city: "Carmel Valley", state: "CA", postalCode: "93924", country: "United States" }
+//3 { addressLineOne: "10 Flight Rd", addressLineTwo: null, city: "Carmel Valley", state: "CA", postalCode: "93924", country: "United States" }
+//4 { addressLineOne: "46800 CA-1", addressLineTwo: null, city: "Big Sur", state: "CA", postalCode: "93920", country: "United States" }
+//5 { addressLineOne: "33200 E Carmel Valley Rd", addressLineTwo: null, city: "Carmel Valley", state: "CA", postalCode: "93924", country: "United States" }
+//6 { addressLineOne: "4TH Ave And Dolores St", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93921", country: "United States" }
+//7 { addressLineOne: "Ocean Ave & Torres St", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93923", country: "United States" }
+//8 { addressLineOne: "Junipero St & 8th Ave", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93923", country: "United States" }
+//9 { addressLineOne: "26270 Dolores St", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93923", country: "United States" }
+//10 { addressLineOne: "San Antonio & Eighth St", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93921", country: "United States" }
+//11 { addressLineOne: "Ocean Ave & Camino Real", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93923", country: "United States" }
+//12 { addressLineOne: "Camino Real & 8th Ave", addressLineTwo: null, city: "Carmel", state: "CA", postalCode: "93921", country: "United States" }
--- backend/seedData/Images.js
+++ backend/seedData/Images.js
@@ -1,1 +1,3 @@
 // { imgUrl: "url", lodgingId: "#", createdAt: new Date(), updatedAt: new Date() },
+// { imgUrl: "http://carmelriverinn.com/wp-content/uploads/cottage-39-11-scaled.jpg", lodgingId: "1", createdAt: new Date(), updatedAt: new Date() },
+// { imgUrl: "url", lodgingId: "#", createdAt: new Date(), updatedAt: new Date() },
