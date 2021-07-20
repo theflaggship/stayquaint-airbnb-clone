3a4d4935e35f98274a5d352be17fbab9cdb99b7c
Monte <36746765+theflaggship@users.noreply.github.com>
2021-07-20 16:37:17 -0700

    get by category api backend working

--- backend/routes/api/lodgings.js
+++ backend/routes/api/lodgings.js
@@ -15,16 +15,15 @@ router.get('/', asyncHandler(async (req, res) => {
 
 //TODO Get all lodgings by category
 
-// router.get('/:category', asyncHandler(async (req, res) => {
-//     const category = await Category.findById(req.params.category);
-//     const lodgings = await Lodging.findAll({
-//         where: {
-//             categoryId: category.id
-//         },
-//         include: Category, Images, Reviews, Address
-//     });
-//     return res.json(lodgings)
-// }));
+router.get('/:categoryId', asyncHandler(async (req, res) => {
+    const category = await Category.findByPk(req.params.categoryId)
+    const lodgings = await Lodging.findAll({
+        where: {
+            categoryId: category.id
+        },
+    });
+    return res.json(lodgings)
+}));
 
 //TODO Add new lodging
 // router.post('/', requireAuth, asyncHandler(async (req, res) => {
